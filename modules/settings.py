69bc1af0cefdd73ce1b3a83108cbd7a4da4442f4
Victor Borzov <borzov@gosniias.ru>
2020-08-12 18:28:22 +0300

    fixed bug with empty admin user id

--- modules/settings.py
+++ modules/settings.py
@@ -7,20 +7,20 @@ def get_bot_token():
 	section = "Tokens"
 	option = "Bot"
 	if not config.has_option(section, option):
-		raise ("config file doesn't contain {}:{}".format(section, option))
+		raise Exception("config file doesn't contain {}:{}".format(section, option))
 	return config[section][option]
 
 def get_owm_token():
 	section = "Tokens"
 	option = "OpenWeatherMap"
 	if not config.has_option(section, option):
-		raise ("config file doesn't contain {}:{}".format(section, option))
+		raise Exception("config file doesn't contain {}:{}".format(section, option))
 	return config[section][option]
 
 def get_admin_id():
 	section = "Tokens"
 	option = "Admin"
 	if not config.has_option(section, option):
-		raise ("config file doesn't contain {}:{}".format(section, option))
+		return 0
 	return config[section][option]
 		
